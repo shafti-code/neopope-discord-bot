0893c63e62a0f15edf9db8ab27a187052291de65
shafti-code <shafti.code@gmail.com>
2026-03-08 21:47:09 +0100

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { NecordModule } from 'necord';
+import { IntentsBitField } from 'discord.js';
 import { StatusController } from 'modules/status/status.controller';
+import { AppUpdate } from 'app.update';
 
 @Module({
-  imports: [],
+  imports: [
+    NecordModule.forRoot({
+      token: process.env["DISCORD_TOKEN"]!,
+      intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.DirectMessages
+      ]
+    })
+  ],
   controllers: [StatusController],
+  providers: [AppUpdate]
 })
 export class AppModule {}
